0b026a389e91aee1b849ba8cca009df3afbf293e
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2015-03-13 21:49:43 +0530

    fixed the issues with initialization

--- eventhub.js
+++ eventhub.js
@@ -3,21 +3,24 @@ module.exports = function(RED) {
 
 	var eventHubClient = require("event-hub-client");
 	
-    function LogEntriesNode(config) {
+    function EventHubNode(config) {
         RED.nodes.createNode(this,config);
 		this.server = RED.nodes.getNode(config.server);
 		
+        console.log(JSON.stringify(config));
+
 		var client = eventHubClient.restClient(
-        serviceBusNamespace,
-        eventHubName,
-        sharedAccessKeyName,
-        sharedAccessKey);
+        config.serviceBusNamespace,
+        config.eventHubName,
+        config.sharedAccessKeyName,
+        config.sharedAccessKey);
     
+        console.log("Initializing the evenhub node.");
 
         this.on('input', function(msg) {
 			//for debug: console.log('logentries:' + JSON.stringify(msg));
 			//client.sendMessage(msg);
-            console.log('logentries:' + JSON.stringify(msg));
+            console.log('message:' + JSON.stringify(msg));
         });
         
 		this.on("close", function () {
@@ -29,15 +32,17 @@ module.exports = function(RED) {
             }
         });
     }
-    RED.nodes.registerType("eventhub in",LogEntriesNode);
+    
+    RED.nodes.registerType("eventhub", EventHubNode);
 	
-	function RemoteServerNode(config) {
-        RED.nodes.createNode(this,config);
-        this.serviceBusNamespace = config.serviceBusNamespace;
-        this.eventHubName = config.eventHubName;
-		this.sharedAccessKeyName = config.sharedAccessKeyName;
-		this.sharedAccessKey = config.sharedAccessKey;
-    }
-    RED.nodes.registerType("eventhub",RemoteServerNode);
+	// function RemoteServerNode(config) {
+
+ //        RED.nodes.createNode(this,config);
+ //        this.serviceBusNamespace = config.serviceBusNamespace;
+ //        this.eventHubName = config.eventHubName;
+	// 	this.sharedAccessKeyName = config.sharedAccessKeyName;
+	// 	this.sharedAccessKey = config.sharedAccessKey;
+ //    }
+ //    RED.nodes.registerType("eventhub",RemoteServerNode);
 }
 
