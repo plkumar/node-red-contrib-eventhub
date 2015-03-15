8cdf7d61685a179ef49ddf6026d5d9179f3f4ff3
Lakshman Kumar Peethani <lakshman@nzentech.com>
2015-03-15 07:56:36 +0530

    moving the evenhub client to amqp1.0 module

--- eventhub.js
+++ eventhub.js
@@ -5,31 +5,48 @@ module.exports = function(RED) {
 	
     function EventHubNode(config) {
         RED.nodes.createNode(this,config);
-		this.server = RED.nodes.getNode(config.server);
-		
+		//this.server = RED.nodes.getNode(config.server);
         //console.log(JSON.stringify(config));
 
-		var client = eventHubClient.restClient(
-        config.serviceBusNamespace,
-        config.eventHubName,
-        config.sharedAccessKeyName,
-        config.sharedAccessKey);
+		// var client = eventHubClient.restClient(
+        //       config.serviceBusNamespace,
+        //       config.eventHubName,
+        //       config.sharedAccessKeyName,
+        //       config.sharedAccessKey);
     
-        console.log("Initializing the evenhub node.");
+        console.log("Initializing the eventhub node.");
+        console.log(JSON.stringify(config));
+
+        var sbus = require('node-sbus-amqp10');
+            var hub = sbus.EventHubClient(config.serviceBusNamespace,
+                config.eventHubName,
+                config.sharedAccessKeyName,
+                config.sharedAccessKey);
 
         this.on('input', function(msg) {
-			client.sendMessage(JSON.stringify(msg), null ,function(err,statusCode){
-                if(err) {
-                    console.log(err);
-                    console.log(statusCode);
-                }
+			
+            // client.sendMessage(JSON.stringify(msg), null ,function(err,statusCode){
+            //     if(err) {
+            //         console.log(err);
+            //         console.log(statusCode);
+            //     }
+            // });
+
+            var partitionKey = Math.floor(Math.random() * 10000).toString();
+
+            console.log('message:' + JSON.stringify(msg));
+
+            hub.send(msg, partitionKey, function(tx_err) {
+                console.log(tx_err); 
             });
-            //console.log('message:' + JSON.stringify(msg));
+            
         });
         
 		this.on("close", function () {
             try {
-                //delete client;
+                hub = null;
+                sbus = null;
+                
                 console.log("received close event");
 
             } catch (error) {
@@ -39,4 +56,3 @@ module.exports = function(RED) {
     
     RED.nodes.registerType("eventhub", EventHubNode);
 }
-
